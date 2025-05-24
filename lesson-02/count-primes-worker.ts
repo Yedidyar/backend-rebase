c4e9686fca39e86e7ab589618c2ec3887ab1ca2d
yedidyar-ni <yedidya.r@next-insurance.com>
2025-05-24 17:22:02 +0300

    Add worker thread implementation for prime counting

--- lesson-02/count-primes-worker.ts
+++ lesson-02/count-primes-worker.ts
@@ -1,16 +1,16 @@
 import { parentPort } from "node:worker_threads";
 
-function isPrime(num) {
+function isPrime(num: number) {
   for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
     if (num % i === 0) return false;
   }
   return num > 1;
 }
 
-function countPrimes(numbers) {
+function countPrimes(numbers: number[]) {
   let count = 0;
   for (let i = 0; i < numbers.length; i++) {
-    if (isPrime(numbers[i])) {
+    if (isPrime(numbers[i]!)) {
       count++;
     }
   }
