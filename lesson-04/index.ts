2f3ec68925d87a2481190ed494ee537a74c6f196
yedidyar-ni <yedidya.r@next-insurance.com>
2025-05-29 12:35:25 +0300

    fix: wrap fetch call in try-catch to handle errors gracefully

--- lesson-04/index.ts
+++ lesson-04/index.ts
@@ -3,11 +3,14 @@ import Fastify from "fastify";
 const fastify = Fastify();
 
 fastify.get("*", async (request, replay) => {
-  const res = await fetch(`${request.url}`, {
-    headers: { ...request.headers, Host: request.host },
-  });
-
-  return res;
+  try {
+    const res = await fetch(`${request.url}`, {
+      headers: { ...request.headers, Host: request.host },
+    });
+    return res;
+  } catch {
+    replay.status(502).send();
+  }
 });
 
 const start = async () => {
