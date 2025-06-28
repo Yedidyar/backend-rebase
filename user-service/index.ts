dcbfa243eb375f29efe3a923a7af87c75df99e93
אריאל שולמן <arielshulman29@ryls-MacBook-Pro-2.local>
2025-06-28 12:48:46 +0300

    ref: move users to userRepository

--- user-service/index.ts
+++ user-service/index.ts
@@ -1,63 +1,10 @@
 import Fastify from "fastify";
 import { config } from "./config.ts";
 import { createLogger } from "./logger/index.ts";
-import { Pool } from "pg";
 
 export const logger = createLogger("user-service");
 
 const fastify = Fastify();
-
-export interface UserDto {
-  id: string;
-  email: string;
-  full_name: string;
-  joined_at: Date;
-  deleted_since?: Date | null;
-}
-
-export class UserRepository {
-  #pool: Pool;
-  constructor(pool: Pool) {
-    this.#pool = pool;
-  }
-
-  async #getSession() {
-    const session = await this.#pool.connect();
-    session.connect();
-    return {
-      session,
-      [Symbol.asyncDispose]: async () => {
-        session.release();
-      },
-    };
-  }
-
-  async upsert(user: UserDto): Promise<UserDto> {
-    const upsertQuery = `
-      INSERT INTO users (id, email, full_name, joined_at, deleted_since)
-      VALUES ($1, $2, $3, $4, $5)
-      ON CONFLICT (id) 
-      DO UPDATE SET
-        email = EXCLUDED.email,
-        full_name = EXCLUDED.full_name,
-        joined_at = EXCLUDED.joined_at,
-        deleted_since = EXCLUDED.deleted_since
-      RETURNING *;
-    `;
-
-    await using session = await this.#getSession();
-
-    const result = await session.session.query(upsertQuery, [
-      user.id,
-      user.email,
-      user.full_name,
-      user.joined_at,
-      user.deleted_since || null,
-    ]);
-
-    return result.rows[0] as UserDto;
-  }
-}
 
 /**
  * Run the server!
---
+++ user-service/repositories/users.ts
@@ -0,0 +1,70 @@
+
+import { Pool } from "pg";
+import { logger } from '../index.ts';
+
+export interface UserDto {
+    id: string;
+    email: string;
+    full_name: string;
+    joined_at: Date;
+    deleted_at?: Date | null;
+}
+
+class 
+
+export class UserRepository {
+    #pool: Pool;
+    constructor(pool: Pool) {
+        this.#pool = pool;
+    }
+
+    async #getSession() {
+        const session = await this.#pool.connect();
+        session.connect();
+        return {
+            session,
+            [Symbol.asyncDispose]: async () => {
+                session.release();
+            },
+        };
+    }
+
+    async upsert(user: UserDto) {
+        try {
+            const upsertQuery = `
+        INSERT INTO users (id, email, full_name, joined_at, deleted_at)
+        VALUES ($1, $2, $3, $4, $5)
+        ON CONFLICT (email) 
+        DO UPDATE SET
+          email = EXCLUDED.email,
+          full_name = EXCLUDED.full_name,
+          deleted_at = EXCLUDED.deleted_at
+        RETURNING id, name, email, full_name, joined_at, 
+            (users.deleted_at is NULL) as already_exists, 
+            (xmax = 0) as is_insert;
+      `;
+            await using session = await this.#getSession();
+            const { rows } = await session.session.query(upsertQuery, [
+                user.id,
+                user.email,
+                user.full_name,
+                user.joined_at,
+                null,
+            ]);
+
+            const { id, full_name, email, joined_at, already_exists, is_insert } = rows[0];
+            const action = 'UPSERT USER';
+
+            if (is_insert) {
+                logger.info({ action, userId: id, message: 'new user was created' })
+            } else if (already_exists) {
+                logger.info({ action, userId: id, message: 'user is already active' })
+            } else {
+                logger.info({ action, userId: id, message: 'user was reactivated' })
+            }
+            return { full_name, email, joined_at }
+        } catch (err) {
+
+        }
+    }
+}
