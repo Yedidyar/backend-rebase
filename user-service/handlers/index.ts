1e543e53e203c61052abb40926fb20f1c999036a
אריאל שולמן <arielshulman29@ryls-MacBook-Pro-2.local>
2025-06-28 13:42:17 +0300

    add: handler

--- user-service/handlers/index.ts
+++ user-service/handlers/index.ts
@@ -1,15 +1,34 @@
 import type { FastifyReply, FastifyRequest, FastifyInstance } from "fastify";
+import { UserService } from "../services/user.service.ts";
 
-export type UserRequest = FastifyRequest<{
+export type GetUserRequest = FastifyRequest<{
   Params: { email: string };
 }>;
 
+export type CreateOrUpdateUserRequest = FastifyRequest<{
+  Params: { email: string; fullName: string };
+}>;
+
 export async function getUserHandler(
-  request: UserRequest,
+  request: GetUserRequest,
   reply: FastifyReply,
 ) {
   const { email } = request.params;
   const user = await request.server.userRepository.getUser(email);
+
+  if (!user) {
+    return reply.status(404).send({ error: "User not found" });
+  }
+
+  return reply.send(user);
+}
+
+export async function saveOrCreateUserHandler(
+  request: CreateOrUpdateUserRequest,
+  reply: FastifyReply,
+) {
+  const { email, fullName } = request.params;
+  const user = await request.server.userService;
 
   if (!user) {
     return reply.status(404).send({ error: "User not found" });
